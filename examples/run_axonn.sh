0ddd24f1fbed8ce6a3805e1b14c636b6d28a0d3f
Siddharth Singh <siddharth9820@gmail.com>
2023-11-07 18:00:29 -0500

    format bash script

--- examples/run_axonn.sh
+++ examples/run_axonn.sh
@@ -4,24 +4,27 @@
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-
 NNODES=$SLURM_JOB_NUM_NODES
-GPUS=$(( NNODES * 4 ))
+GPUS_PER_NODE=4 ## change as per your machine
+GPUS=$(( NNODES * GPUS_PER_NODE )) 
+
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=29500
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-export NCCL_NET_GDR_LEVEL=PHB
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-export CUDA_VISIBLE_DEVICES=3,2,1,0
-export NCCL_CROSS_NIC=1
-export NCCL_SOCKET_IFNAME=hsn
-export NCCL_NET="AWS Libfabric"
-export FI_CXI_RDZV_THRESHOLD=0
-export FI_CXI_RDZV_GET_MIN=0
-export FI_CXI_OFLOW_BUF_SIZE=1073741824
-export FI_CXI_OFLOW_BUF_COUNT=1
+
+# The following args boost bw on Perlmutter
+#export NCCL_NET_GDR_LEVEL=PHB
+#export CUDA_DEVICE_MAX_CONNECTIONS=1
+#export CUDA_VISIBLE_DEVICES=3,2,1,0
+#export NCCL_CROSS_NIC=1
+#export NCCL_SOCKET_IFNAME=hsn
+#export NCCL_NET="AWS Libfabric"
+#export FI_CXI_RDZV_THRESHOLD=0
+#export FI_CXI_RDZV_GET_MIN=0
+#export FI_CXI_OFLOW_BUF_SIZE=1073741824
+#export FI_CXI_OFLOW_BUF_COUNT=1
 
 
+# data/checkpoint args
 DATA_DIR="${SCRATCH}/gpt_data"
 CHECKPOINT_PATH="${DATA_DIR}/checkpoints"
 VOCAB_FILE="${DATA_DIR}/gpt2-vocab.json"
